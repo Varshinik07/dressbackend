77a74c34115c31bc4e833c92509baede2df8229d
Varshini <varshuk01@gmail.com>
2022-10-19 22:00:18 +0530

    message

---
+++ src/main/java/com/nestdigital/dressbackendapp/DressBackendAppApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.dressbackendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DressBackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DressBackendAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestdigital/dressbackendapp/DressBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.dressbackendapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DressBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
